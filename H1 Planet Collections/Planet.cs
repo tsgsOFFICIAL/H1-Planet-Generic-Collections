b8cf2e00ec8ad14f8d7637a457968b216c163704
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-25 12:37:20 +0200

    Add project files.

---
+++ H1 Planet Collections/Planet.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace H1_Planet_Collections
+    {
+    class Planet
+        {
+        #region Fields
+        private bool ringSystem;
+        private byte gravity;
+        private byte numberOfMoons;
+        private short density;
+        private short meanTemperature;
+        private float lengthOfDay;
+        private float orbitalPeriod;
+        private float rotationPeriod;
+        private float orbitalVelocity;
+        private float distanceFromSun;
+        private decimal mass;
+        private decimal diameter;
+        #endregion
+
+        #region Properties
+        public enum Planets 
+            {
+            Mercury,
+            Venus,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Uranus,
+            Neptune,
+            Pluto
+            }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Constructor for Planet class, taking a string as a parameter
+        /// </summary>
+        /// <param name="planet">Specifies what planet to create</param>
+        public Planet(Planets planet)
+            {
+            switch (planet)
+                {
+                case Planets.Mercury:
+                    break;
+                case Planets.Venus:
+                    break;
+                case Planets.Earth:
+                    break;
+                case Planets.Mars:
+                    break;
+                case Planets.Jupiter:
+                    break;
+                case Planets.Saturn:
+                    break;
+                case Planets.Uranus:
+                    break;
+                case Planets.Neptune:
+                    break;
+                case Planets.Pluto:
+                    break;
+                }
+            }
+        #endregion
+        }
+    }
---
+++ H1 Planet Collections/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace H1_Planet_Collections
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+
+            Console.ReadKey(true);
+            }
+        }
+    }
